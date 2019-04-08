ca218d4de202ff8bfae9b223c396e71609de62c4
xiongkai <xiongkai01@wondersgroup.com>
2019-04-08 15:46:55 +0800

    add build,dev

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ function component() {
   var element = document.createElement('div');
   var btn = document.createElement('button');
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = _.join(['Hello', 'webpack','welcome','here'], ' ');
 
   btn.innerHTML = 'Click me and check the console!';
   btn.onclick = printMe;
